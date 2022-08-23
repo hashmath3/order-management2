5cafb444d7cddb99252229dba5ccde3411fc8834
hashmath3 <hashmathbasha@gmail.com>
2022-08-23 14:15:16 +0530

    first comment

---
+++ js/createoder.js
@@ -0,0 +1,49 @@
+const createOrderCard = (orderId) => {
+	let orderCardsWrapper = document.getElementById('order-cards-wrapper')
+
+	let colDiv = document.createElement('div')
+	let cardDiv = document.createElement('div')
+	let cardHeader = document.createElement('div')
+	let cardBody = document.createElement('div')
+	let cardText1 = document.createElement('p')
+	let cardText2 = document.createElement('p')
+	let cardText3 = document.createElement('p')
+	let statusBadge = document.createElement('span')
+	let cardFooter = document.createElement('div')
+
+	colDiv.classList = 'col-md-3 order-status-card'
+	cardDiv.classList = 'card text-center'
+	cardHeader.classList = 'card-header'
+	cardBody.classList = 'card-body'
+	cardText1.classList = 'card-text'
+	cardText2.classList = 'card-text'
+	cardText3.classList = 'card-text'
+	statusBadge.classList = 'badge rounded-pill bg-success'
+	cardFooter.classList = 'card-footer'
+
+	cardHeader.innerHTML = `Order ID: <b>${orderId}</b>`
+	cardText1.innerText = 'Medium Size Pizza - 2 Nos'
+	cardText2.innerHTML = 'Bill Amount: <b>$22</b>'
+	statusBadge.innerText ='First layer of cheeze added'
+	cardFooter.innerText = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit"
+  });
+
+	statusBadge.id = orderId
+
+	cardDiv.appendChild(cardHeader)
+	cardBody.appendChild(cardText1)
+	cardBody.appendChild(cardText2)
+	cardBody.appendChild(cardText3)
+	cardText3.appendChild(statusBadge)
+	cardDiv.appendChild(cardBody)
+	cardDiv.appendChild(cardFooter)
+	colDiv.appendChild(cardDiv)
+	orderCardsWrapper.appendChild(colDiv)
+}
+
